49c6f53208c609d628294ad6597997c4c9943ffe
Severino de Souza Souza <severino_souza@outlook.com>
2021-07-29 22:17:30 -0400

    Desenvolvimento da função listar filmes por título.

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -23,3 +23,24 @@ exports.create = (res) => {
       console.log("Erro ao cadastrar o movie.");
     });
 };
+
+//Busca de filmes por titulo
+exports.findMoviesTitle = (req, res) => {
+  const filter = "%" + req.body.title + "%";
+
+  Movie.findAll({
+    where: {
+      title: {
+        [Sequelize.Op.like]: filter,
+      },
+    },
+  })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Erro ao listar o filme.",
+      });
+    });
+};
